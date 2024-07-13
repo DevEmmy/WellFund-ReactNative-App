4a627335fbede16ed720db9179e34ff3cf30da5a
web-tranquility <eolaosebikan60@gmail.com>
2024-07-14 00:42:49 +0100

    added oustanding category

--- app/(main)/main.jsx
+++ app/(main)/main.jsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, TextInput, ScrollView, Image } from 'react-native'
+import { View, Text, SafeAreaView, TextInput, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/Feather';
 import ProductCard from '../../Components/Product/ProductCard';
@@ -14,7 +14,7 @@ const Main = () => {
 
           <View>
             <Text className="text-[18px]">Greetings 👋</Text>
-            <Text className="text-[20px] font-semibold">Emmanuel Olaosebikan</Text>
+            <Text className="text-[26px] font-bold">Emmanuel Olaosebikan</Text>
           </View>
         </View>
 
@@ -24,22 +24,52 @@ const Main = () => {
       </View>
 
       <View className="mx-[5%] bg-black p-6 py-10 rounded-2xl my-10 text-white shadow-lg">
-        <Text className="text-white text-[22px] font-semibold">What are you getting from us today</Text>
+        <Text className="text-white text-[22px] font-bold">What are you getting from us today</Text>
         <Text className="text-white text-[16px]">Discover your daily needs here.</Text>
       </View>
-      
 
-      <View className="flex flex-col gap-y-2 mx-[5%]">
+
+      <View className="flex flex-col  mx-[5%]">
         <View className="">
-          <Text className="text-[20px] text-black font-semibold">Categories</Text>
+          <Text className="text-[26px] text-black font-bold">Oustanding Loans</Text>
+        </View>
+        <ScrollView horizontal={false} className="flex flex-col gap-y-3 overflow-scroll">
+          {
+            [1, 2].map((item, i) => {
+              return (
+                <View className="flex gap-y-3 justify-between flex-row items-center" key={i}>
+                  <View className="flex flex-row gap-x-3 items-center">
+                    <Image source={{ uri: "https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg?crop=1xw:1xh;center,top&resize=1200:*" }} className="w-[120px] h-[80px] rounded-xl " />
+
+                    <View>
+                      <Text className=" text-[18px] font-light ">Groceries </Text>
+                      <Text className=" text-[18px] text-gray-400 font-light">picked up by 12/24 </Text>
+                    </View>
+                  </View>
+
+                  <TouchableOpacity>
+                    <Text className="bg-gray-200 p-2 rounded-full">View Details</Text>
+                  </TouchableOpacity>
+
+                </View>
+              )
+            })
+          }
+        </ScrollView>
+      </View>
+
+
+      <View className="flex flex-col gap-y-2 mt-10 mx-[5%]">
+        <View className="">
+          <Text className="text-[26px] text-black font-bold">Categories</Text>
         </View>
         <ScrollView horizontal={true} className="flex flex-row gap-x-5 overflow-scroll">
           {
             [1, 2, 3, 4, 5].map((item, i) => {
               return (
-                <View className="flex flex-col items-center justify-center" key={i}>
-                  <Image source={{ uri: "https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg?crop=1xw:1xh;center,top&resize=1200:*" }} className="w-[80px] h-[80px] rounded-full " />
-                  <Text className="text-center font-bold ">Groceries </Text>
+                <View className="flex gap-y-3 flex-col items-center justify-center" key={i}>
+                  <Image source={{ uri: "https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg?crop=1xw:1xh;center,top&resize=1200:*" }} className="w-[120px] h-[120px] rounded-full " />
+                  <Text className="text-center text-[18px] font-bold ">Groceries </Text>
 
                 </View>
               )
@@ -50,7 +80,7 @@ const Main = () => {
 
       <ScrollView className="flex flex-col gap-y-1 my-5">
         <View className="">
-          <Text className="text-[20px] text-black font-semibold px-5">Recently Added</Text>
+          <Text className="text-[26px] text-black font-bold px-5">Recently Added</Text>
         </View>
         <View className="flex flex-1 flex-row flex-wrap gap-y-3">
           {
@@ -67,7 +97,7 @@ const Main = () => {
 
       <ScrollView className="flex flex-col gap-y-1 my-5">
         <View className="">
-          <Text className="text-[20px] text-black font-semibold px-5">Featured Posts</Text>
+          <Text className="text-[26px] text-black font-bold px-5">Featured Posts</Text>
         </View>
         <View className="flex flex-1 flex-row flex-wrap gap-y-3">
           {
