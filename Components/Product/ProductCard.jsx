3c1e41f957689a5df642014bf2fe6f92b75cd6b1
web-tranquility <eolaosebikan60@gmail.com>
2024-07-14 00:42:19 +0100

    increased sizes around product card

--- Components/Product/ProductCard.jsx
+++ Components/Product/ProductCard.jsx
@@ -6,12 +6,12 @@ export default function ProductCard() {
     <View className="bg-white shadow p-2 rounded-2xl mx-2" >
       <Image source={{ uri: "https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg?crop=1xw:1xh;center,top&resize=1200:*" }} className="w-full h-[200px] rounded-xl" />
 
-      <View className="py-2">
-        <Text className="text-[18px] font-semibold">Rice</Text>
-        <Text className="text-[18px]">₦ 20,000
+      <View className="py-2 flex flex-col gap-y-1">
+        <Text className="text-[22px] font-bold">Rice</Text>
+        <Text className="text-[16px] text-gray-400">2% reduced starched rice, 1 plastic
         </Text>
-        <Text className="">Refund Span - 3 M</Text>
-        <TouchableOpacity className="bg-primary2 rounded-lg p-2 text-center">
+        <Text className="text-[16px] text-gray-400">Repayment in 5 days</Text>
+        <TouchableOpacity className="bg-primary2 rounded-lg p-3 text-center">
           <Text className="text-white font-bold text-center text-[16px]">Add to Cart</Text>
         </TouchableOpacity>
       </View>
